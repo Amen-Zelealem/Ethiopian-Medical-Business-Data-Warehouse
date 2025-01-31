ac72eab45b25934af61744af8d296f457b04a31f
Amen Zelealem <amenzelealemtadese@gmail.com>
2025-01-31 20:09:40 +0300

    Data Transformation using DBT

---
+++ Medical_Business_Data_Warehouse/models/example/select_cleaned_data.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='table') }}
+
+with source_data as (
+    select
+        channel_name,
+        channel_title,
+        text_date,
+        message,
+        emoji_used
+    from {source_data}
+)
+
+select *
+from source_data
