e3c134916c082421b056f54d22b65de2499a8964
Amen Zelealem <amenzelealemtadese@gmail.com>
2025-02-01 10:36:48 +0300

    Updated Models

--- Medical_Business_Data_Warehouse/models/example/convert_text_date_to_message_date.sql
+++ Medical_Business_Data_Warehouse/models/example/convert_text_date_to_message_date.sql
@@ -4,17 +4,16 @@ with source_data as (
     select
         channel_name,
         channel_title,
-        text_date::timestamp,  
+        text_date::timestamp as message_date,  -- Converting text_date to message_date
         message,
         emoji_used
-    from {{ ref('raw_messages') }}  
-    where message is not null       
+    from {{ ref('select_cleaned_data') }}  -- Referencing the select_cleaned_data model
 )
 
 select 
     channel_name,
     channel_title,
-    text_date,
+    message_date,
     message,
     emoji_used
 from source_data
